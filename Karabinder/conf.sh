9a759cdf55107556b7bb10c9d5224bbea5623b3f
Yoshinori ISOGAI <isogai_yoshinori@kakaku.com>
2015-06-22 13:38:08 +0900

    F12/F13キーを復活させた。

--- Karabinder/conf.sh
+++ Karabinder/conf.sh
@@ -2,42 +2,38 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.commandL2controlL 1
-/bin/echo -n .
-$cli set remap.doublepresscommandQ 1
-/bin/echo -n .
-$cli set remap.jis_unify_kana_eisuu_to_commandR 1
-/bin/echo -n .
-$cli set remap.app_firefox_controlK2commandK 1
-/bin/echo -n .
-$cli set remap.app_firefox_controlL2commandL 1
-/bin/echo -n .
-$cli set repeat.wait 20
-/bin/echo -n .
-$cli set remap.controlL2commandL 1
-/bin/echo -n .
-$cli set remap.jis_backquote_kana_eisuu 1
-/bin/echo -n .
-$cli set remap.pc_pause2eject 1
-/bin/echo -n .
-$cli set remap.jis_underscore2backslash 1
-/bin/echo -n .
-$cli set remap.jis_eisuu2controlL 1
-/bin/echo -n .
-$cli set option.drop_slowexpose 1
-/bin/echo -n .
 $cli set remap.pc_application2rightclick 1
 /bin/echo -n .
-$cli set remap.pc_printscreen2f12 1
-/bin/echo -n .
-$cli set remap.pclikehomeend 1
+$cli set repeat.initial_wait 0
 /bin/echo -n .
 $cli set remap.jis_kana2shiftL 1
 /bin/echo -n .
-$cli set remap.pc_insert2controlR 1
+$cli set remap.controlL2commandL 1
+/bin/echo -n .
+$cli set remap.jis_eisuu2controlL 1
+/bin/echo -n .
+$cli set remap.pclikehomeend 1
+/bin/echo -n .
+$cli set remap.commandL2controlL 1
+/bin/echo -n .
+$cli set remap.app_firefox_controlL2commandL 1
+/bin/echo -n .
+$cli set remap.pc_pause2eject 1
+/bin/echo -n .
+$cli set remap.jis_unify_kana_eisuu_to_commandR 1
+/bin/echo -n .
+$cli set repeat.wait 20
 /bin/echo -n .
 $cli set remap.app_firefox_controlT2commandT 1
 /bin/echo -n .
-$cli set repeat.initial_wait 0
+$cli set remap.doublepresscommandQ 1
+/bin/echo -n .
+$cli set remap.pc_insert2controlR 1
+/bin/echo -n .
+$cli set remap.jis_underscore2backslash 1
+/bin/echo -n .
+$cli set remap.jis_backquote_kana_eisuu 1
+/bin/echo -n .
+$cli set remap.app_firefox_controlK2commandK 1
 /bin/echo -n .
 /bin/echo
